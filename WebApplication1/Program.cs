44c0996e78e38dee9dc3b117aa2c418a9d352eb2
Damian Reboredo <reboredodamian@hotmail.com>
2021-12-10 05:29:21 -0300

    asd

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -13,6 +13,12 @@ namespace WebApplication1
     {
         public static void Main(string[] args)
         {
+            int target = -5;
+            int num = 3;
+
+            target = -num;  // Noncompliant; target = -3. Is that really what's meant?
+            target = +num; // Noncompliant; target = 3
+
             CreateHostBuilder(args).Build().Run();
         }
 
